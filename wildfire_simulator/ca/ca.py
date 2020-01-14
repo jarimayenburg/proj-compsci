b2559ce08627b20d7f74f4d5ebad6acab8cee81a
Jari Maijenburg <jari.maijenburg@ivido.nl>
2020-01-14 17:05:08 +0100

    PEP8 stuff

--- wildfire_simulator/ca/ca.py
+++ wildfire_simulator/ca/ca.py
@@ -7,8 +7,12 @@ class CA:
     def __init__(self, grid_size):
         self.grid_size = grid_size
         self.grid = self.init_grid(grid_size)
+        self.running = False
 
     def init_grid(self, size):
         # TODO build the initial grid from a file
 
         return [[0] * size] * size
+
+    def step(self):
+        self.running = True
