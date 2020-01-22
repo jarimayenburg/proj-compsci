bff30691a92d954537d559013c3c81deb3722ccc
youri <yourimoll1994@gmail.com>
2020-01-22 14:19:16 +0100

    added pdens and pveg as described in the paper

--- wildfire_simulator/ca/evolution_rules.py
+++ wildfire_simulator/ca/evolution_rules.py
@@ -6,3 +6,29 @@ class NNEvolutionRule:
 
     def evolve(self, cell, neighborhood):
         """Evolve a cell in a CA."""
+
+    def pburn(self):
+        """Probability that a cell will start buring"""
+        # TODO: Implementation
+
+    def pveg(self, veg):
+        """ """
+        if veg is noveg:
+            return -1
+        else if veg is agriculture:
+            return -0.4
+        else if veg is forest or veg is Shrubland:
+            return 0.4
+        else:
+            raise Error
+    def pdens(self, veg):
+        if veg is noveg:
+                return -1
+        else if veg is sparse:
+            return -0.3
+        else if veg is normal:
+            return 0
+        else if veg is dense:
+            return 0.3
+        else:
+            raise Error
